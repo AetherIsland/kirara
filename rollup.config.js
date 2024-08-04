559c143f150aa56b89a84e9a199ba730fc19d317
Aether Chen <15167799+chenjunyu19@users.noreply.github.com>
2024-08-04 17:54:42 +0800

    🔧 change rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import { nodeResolve } from '@rollup/plugin-node-resolve'; 
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import terser from '@rollup/plugin-terser';
 
@@ -9,17 +9,13 @@ export default {
     input: './src/main.ts',
     output: {
         dir: './dist',
-        format: 'esm',
         sourcemap: true
     },
     plugins: [
         nodeResolve(),
         typescript({
-            /**
-             * @see https://github.com/rollup/plugins/issues/1583
-             * @see @tsconfig/node-lts/tsconfig.json
-             */
-            module: 'node16'
+            module: 'esnext',
+            moduleResolution: 'bundler'
         }),
         terser()
     ]
