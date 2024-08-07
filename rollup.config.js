b574562f83a610d3548abbb6965de90ec0a4effb
Aether Chen <15167799+chenjunyu19@users.noreply.github.com>
2024-08-07 21:41:14 +0800

    🧱 add more rollup plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,28 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import terser from '@rollup/plugin-terser';
+import progress from 'rollup-plugin-progress';
+import sizes from 'rollup-plugin-sizes';
 
 /**
  * @type {import('rollup').RollupOptions}
  */
 export default {
-    input: './src/main.ts',
+    input: 'src/main.ts',
     output: {
-        dir: './dist',
+        dir: 'dist',
         sourcemap: true
     },
     plugins: [
         nodeResolve(),
+        commonjs(),
         typescript({
             module: 'esnext',
             moduleResolution: 'bundler'
         }),
-        terser()
+        terser(),
+        progress(),
+        sizes()
     ]
 };
